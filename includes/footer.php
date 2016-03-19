ac1e20ca18c2d860e6b88e421cbe1d45c831baf2
Mario Brusarosco <mariobrusarosco@hotmail.com>
2016-03-19 17:22:46 -0300

    APARENTEMENTE, A PRIMEIRA VALIDAÇÃO NO FRONT-END ESTÁ FUNCIONANDO

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
 
   <!-- <script></script> -->
+  <script src="js/jquery-2.2.1.min.js"></script>
   <script src="js/teste-de-mercado.js"></script>
   <script src="js/NovaNegoc.js"></script>
-  <script src="js/jquery-2.2.1.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
--- js/NovaNegoc.js
+++ js/NovaNegoc.js
@@ -1,8 +1,9 @@
 // ESSA CLASSE FUNCIONA COMO UM OBJETO GLOBAL//
 
 /* ==== PROPIEDADES =====
-   -- htmlModal
+   -- htmlNovoModal
       ==> ARMAZENA O CÓDIGO HTML PARA UM MODAL DE NOVA NEGOCIAÇÃO.
+        //AQUI PROVAVELMETE EXISTE UM JEITO MELHOR DO QUE ESSE. DESSE JEITO O BROWSER VAI FAZER UM REQUISIÇÃO A MAIS SÓ PARA UM PEQUENO PEDAÇO DE HTML//
 
   ===== MÉTODOS =====
    -- execHandlers()
@@ -11,18 +12,42 @@
           UMA NOVA NEGOCIAÇÃO
 */
 var NovaNegoc = {
-  htmlModal    : "",
-  abrirModal   : function(){
-                  // var that = this;
-                  //CHECA SE O MODAL JÁ CONTÉM SEU CÓDIGO HTML//
-                  if(!NovaNegoc.htmlModal.length){  //SE NÃO TIVER... INSIRA COM UMA CHAMADA AJAX//
-                    $.ajax
-                    console.log("modal vazio");
-                  }
-                  console.log(NovaNegoc);
-                },
-  execHandlers : function(){
+  validarNegoc   : function(){
+                    var dadosValidos = true,                                        //ESSA VARIÁVEL SERVE PARA CONTROLAR O STATUS DA VALIDAÇÃO//
+                        msgRetorno   = "",                                          //MENSAGEM PARA EXPLICAR STATUS DA VALIDAÇÃO;
+                              $modal = $("main").children("#modal_nova_negoc");     //SELECIONA O MODAL DE UMA NOVA NEGOCIAÇÃO//
+
+                    $("main").find("form").find(".form-control").each(function(){   //SELECIONA O <form>, BUSCA CADA ITEM COM A CLASSE .form-control//
+                      //PARA CADA UM DELES//
+                          if(!$(this).val().length){                                //SE ALGUM CAMPO ESTIVER VAZIO//
+                            dadosValidos = false;                                   //A ENTRADA NÃO DEVE SER ACEITA. STATUS ALTERADO PARA 'FALSO'//
+                            msgRetorno   = "Por favor, preencha todos os campos";   //EXPLICAR O MOTIVO DE ERRO//
+                          }
+                    });
+                      return [dadosValidos,msgRetorno];                             //RETORNA UM ARRAY COM O STATUS E A MENSAGEM//
+                    },
+
+  salvarNegoc    : function(){
+                    var $node = $(this),                                            //ARMAZENA O BOTÃO PARA SALVAR A NEGOCIAÇÃO//
+                       $modal = $("main").children("#modal_nova_negoc");            //SELECIONA O MODAL DE UMA NOVA NEGOCIAÇÃO//
+
+                    $modal.find(".alert").each(function(){ $(this).remove();});     //SE JÁ EXISTIR UM ALERT, ELE SERÁ REMOVIDO//
+
+                    //VALIDA OS DADOS. VERIFICAR O PRIMEIRO ITEM DO ARRAY, QUE É UM VALOR BOOLEANO INDICANTO O RESULTADO DA VALIDAÇÃO//
+                    if(NovaNegoc.validarNegoc()[0]){                                //SE FOR 'true' OS DADOS SERÃO ACEITOS//
+                        document.querySelector("form").reset();                     //ZERA OS CAMPOS DO FORMULÁRIO, PARA QUE O USUÁRIO NÃO PRECISE APAGAR O VALOR ANTIGO//
+                        $modal.modal("hide");                                       //ESCONDER O MODAL PARA EXIBIR UMA MENSAGEM DE SUCESSO AO USUÁRIO//
+                        $("#modal_alertas").modal("show");                          //EXIBIR UMA MENSAGEM COM O STATUS DA INSERÇÃO AO USUÁRIO//
+                    }else{  //SE FOR 'false' OS DADOS NÃO SERÃO ACEITOS//
+                      //INSERE NO MODAL UM AVISO DE ERROR NA INSERÇÃO, ANTES DO BOTÃO DE SALVAR E FECHAR//
+                      $("<div class='alert alert-danger text-center'>" + NovaNegoc.validarNegoc()[1]+ "</div>").insertBefore($node.prev());
+                    }
+
+                  },
+  execHandlers  : function(){
                     //EVENTO PARA O CLIQUE NO BOTÃO DE 'NOVA NEGOCIAÇÃO'//
-                    $("main").on("click","#nova_negoc_btn",this.abrirModal);
+                    // $("main").on("click","#nova_negoc_btn",this.abrirModal);
+
+                    $("main").on("click","#salvar_nova_negoc",this.salvarNegoc);
                 }
 }
