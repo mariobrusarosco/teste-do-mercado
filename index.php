d7defb8d7aed3eb4ea46e04576f811e1883951e9
Mario Brusarosco <mariobrusarosco@hotmail.com>
2016-03-19 12:23:45 -0300

    CRIADO UM MODAL PARA INSERIR OS DADOS DE UMA NOVA NEGOCIAÇÃO

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
 <main class="container">
 
   <header>
-
     <div id="logo_vale_mobi" class="logo row">
       <div class="col-xs-12 col-lg-6">
         <img class="img-responsive" src="img/logo.png" title="ValeMobi" alt="ValeMobi">
@@ -20,18 +19,32 @@
 
     <nav id="barra_navegacao" class="nav row">
       <div class="col-xs-12">
-        <button id="nova_negoc_btn" class="btn btn-info btn-block center-block">Nova Negociação</button>
+        <button id="nova_negoc_btn" class="btn btn-info btn-block center-block" type="button" data-toggle="modal" data-target="#modal_nova_negoc">Nova Negociação</button>
       </div>
       <div class="col-xs-12">
-        <button id="lista_negoc_btn" class="btn btn-primary btn-block center-block">Listar Negocições</button>
+        <button id="lista_negoc_btn" class="btn btn-primary btn-block center-block" type="button" data-toggle="modal" data-target="#myModal">Listar Negocições</button>
       </div>
     </nav>
-
   </header>
 
-  <section>
+    <div class='modal fade' id='modal_nova_negoc' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>
+      <div class='modal-dialog modal-lg' role='document'>
+        <div class='modal-content'>
+          <div class='modal-header'>
+            <button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+            <h4 class='modal-title' id='myModalLabel'>Nova Negociação</h4>
+          </div>
+          <div class='modal-body'>
 
-  </section>
+          </div>
+          <div class='modal-footer'>
+            <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
+            <button type='button' class='btn btn-primary'>Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
 
 </main>
--- js/NovaNegoc.js
+++ js/NovaNegoc.js
@@ -1,21 +1,28 @@
 // ESSA CLASSE FUNCIONA COMO UM OBJETO GLOBAL//
 
-//MÉTODOS
-  //
-  // execHandlers()//
-    // ESSE MÉTODO SERÁ CHAMADO NO CARREGAMENTO PRINCIPAL DA PÁGINA.
-    // IRÁ INSERIR TODAS AS CAPTURAS DE EVENTOS REFERENTES AO PROCESSO DE INSERIR
-    // UMA NOVA NEGOCIAÇÃO
+/* ==== PROPIEDADES =====
+   -- htmlModal
+      ==> ARMAZENA O CÓDIGO HTML PARA UM MODAL DE NOVA NEGOCIAÇÃO.
 
+  ===== MÉTODOS =====
+   -- execHandlers()
+      ==> ESSE MÉTODO SERÁ CHAMADO NO CARREGAMENTO PRINCIPAL DA PÁGINA.
+          IRÁ INSERIR TODAS AS CAPTURAS DE EVENTOS REFERENTES AO PROCESSO DE INSERIR
+          UMA NOVA NEGOCIAÇÃO
+*/
 var NovaNegoc = {
+  htmlModal    : "",
   abrirModal   : function(){
-                  console.log("works");
+                  // var that = this;
+                  //CHECA SE O MODAL JÁ CONTÉM SEU CÓDIGO HTML//
+                  if(!NovaNegoc.htmlModal.length){  //SE NÃO TIVER... INSIRA COM UMA CHAMADA AJAX//
+                    $.ajax
+                    console.log("modal vazio");
+                  }
+                  console.log(NovaNegoc);
                 },
   execHandlers : function(){
                     //EVENTO PARA O CLIQUE NO BOTÃO DE 'NOVA NEGOCIAÇÃO'//
                     $("main").on("click","#nova_negoc_btn",this.abrirModal);
-                    // $("main").on("click","#nova_negoc_btn",function(){
-                    //
-                    // })
                 }
 }
