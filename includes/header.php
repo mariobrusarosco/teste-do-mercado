a81e0e500fb765bbf484a745fbdaa530c1eb69e7
Mario Brusarosco <mariobrusarosco@hotmail.com>
2016-03-19 11:22:24 -0300

    ADICIONANDO DOIS BOTÕES NA PÁGINA PRINCIPAL. OS BOTÕES AINDA ESTÃO ESTÁTICOS

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
 <html>
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <title>Teste de Mercado - ValeMobi</title>
 
   <link rel="stylesheet" href="css/bootstrap.min.css">
--- index.php
+++ index.php
@@ -8,11 +8,33 @@
   require_once("includes/header.php");
 ?>
 
-<div class="container">
-  <div class="row">
-      <button id="mario" class="btn btn-primary">My Row</button>
-  </div>
-</div>
+<main class="container">
+
+  <header>
+
+    <div id="logo_vale_mobi" class="logo row">
+      <div class="col-xs-12 col-lg-6">
+        <img class="img-responsive" src="img/logo.png" title="ValeMobi" alt="ValeMobi">
+      </div>
+    </div>
+
+    <nav id="barra_navegacao" class="nav row">
+      <div class="col-xs-12">
+        <button id="nova_negoc_btn" class="btn btn-info btn-block center-block">Nova Negociação</button>
+      </div>
+      <div class="col-xs-12">
+        <button id="lista_negoc_btn" class="btn btn-primary btn-block center-block">Listar Negocições</button>
+      </div>
+    </nav>
+
+  </header>
+
+  <section>
+
+  </section>
+
+
+</main>
 
 <?php
   //CARREGA O RODAPÉ DA PÁGINA//
